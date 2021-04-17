75208e123c506c97b925ab6a9d6ead208175b970
Vineeth Avvaru <avvaru95@gmail.com>
2021-04-17 00:13:12 -0400

    Initial setup

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Map from "./MapComponent";
+
+function Main() {
+  return (
+    <div>
+      Main !!
+      <Map />
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Map() {
+  return <div>Map!!</div>;
+}
+
+export default Map;
